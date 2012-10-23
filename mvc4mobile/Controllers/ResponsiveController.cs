0719b5f38ee475af6239e713d1a8c570f899a516
roszman <roszman@gmail.com>
2012-10-23 15:41:24 +0200

    litte cleanup

--- mvc4mobile/Controllers/ResponsiveController.cs
+++ mvc4mobile/Controllers/ResponsiveController.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
-using System.Web.WebPages;
-using System.Web.Http;
 
 namespace mvc4mobile.Controllers
 {
-    public class HomeController : Controller
+    public class ResponsiveController : Controller
     {
         //
-        // GET: /Mobile/
+        // GET: /Responsive/
 
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
