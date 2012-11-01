d37f94ee622404a4ed479b3689c731e61e958959
roszman <roszman@gmail.com>
2012-11-01 20:32:11 +0100

    media queries and few corrections

---
+++ mvc4mobile/Controllers/CSS3MediaQueriesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc4mobile.Controllers
+{
+    public class CSS3MediaQueriesController : Controller
+    {
+        //
+        // GET: /CSS3MediaQueries/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
